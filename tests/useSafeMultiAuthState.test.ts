b2a36b3e0ee04a5d3a3ea3c722585e8c3936b5b2
Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>
2022-12-13 20:20:27 +0000

    feat: add keys test
    Signed-off-by: Hanif Dwy Putra S <99879343+hanifdwyputras@users.noreply.github.com>

--- tests/useSafeMultiAuthState.test.ts
+++ tests/useSafeMultiAuthState.test.ts
@@ -10,10 +10,12 @@ test.before('Declare key', async(t) => {
 })
 
 test('init credentials', async(t) => {
+	t.log('Initializing creds')
 	await t.notThrowsAsync(useSafeMultiAuthState(key, pathResolve(__dirname, 'sessions')))
 })
 
 test('credentials change', async(t) => {
+	t.log('Preparing state')
 	const state = await useSafeMultiAuthState(key, pathResolve(__dirname, 'sessions'))
 	t.not(state, undefined)
 
@@ -28,10 +30,35 @@ test('credentials change', async(t) => {
 })
 
 test('validate credentials change', async(t) => {
+	t.log('Preparing state')
 	const state = await useSafeMultiAuthState(key, pathResolve(__dirname, 'sessions'))
 	t.not(state, undefined)
 
+	t.log(state.state.creds)
 	t.deepEqual(state.state.creds.me?.id, '628381928192')
 	t.deepEqual(state.state.creds.me?.name, 'John Doe')
 	t.is(state.state.creds.platform, 'iOS')
+})
+
+test('key set 123 -> hello', async(t) => {
+	t.log('Preparing state')
+	const state = await useSafeMultiAuthState(key, pathResolve(__dirname, 'sessions'))
+	t.not(state, undefined)
+
+	await t.notThrowsAsync(async() => state.state.keys.set({
+		'session': {
+			'123': 'hello',
+		}
+	}))
+})
+
+test('key get 123 -> hello', async(t) => {
+	t.log('Preparing state')
+	const state = await useSafeMultiAuthState(key, pathResolve(__dirname, 'sessions'))
+	t.not(state, undefined)
+
+	const res = await state.state.keys.get('session', ['123'])
+	t.log(res)
+
+	t.is(res[123], 'hello')
 })
